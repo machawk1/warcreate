533f93dff1f13bcd442b46d97ff78946c4aff385
Mat Kelly <machawk1@gmail.com>
2015-08-26 15:12:06 -0400

    Removed superfluous semicolons

--- js/warc.js
+++ js/warc.js
@@ -8,20 +8,20 @@ Warc = function(srcStr) {
 		this.str = '';
 		function WarcInfoHeader(){var str;
 			addLine('WARC/1.0');
-		};
-		function WarcInfoContent(){};
-		function RequestHeader(){};
-		function RequestContent(){};
-		function MetaDataHeader(){};
-		function MetaDataContent(){};
-		function ResponseHeader(){};
+		}
+		function WarcInfoContent(){}
+		function RequestHeader(){}
+		function RequestContent(){}
+		function MetaDataHeader(){}
+		function MetaDataContent(){}
+		function ResponseHeader(){}
 		function ResponseContent(){
 			function ResponseHTTPHeader(){}
 			var content;
-		};
+		}
 		function addLine(str){
 			this.str += str + '\r\n';
-		};
+		}
 	}
 
 }
