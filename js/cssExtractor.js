43dbd10f058eabaa0885f7837ccb4fb8ecb02cd6
Mat Kelly <me@matkelly.com>
2016-05-04 15:32:16 -0400

    JS standardization

--- js/cssExtractor.js
+++ js/cssExtractor.js
@@ -1,54 +1,55 @@
-function findEmbeddedCSSURIs() {
-  var foundCSSFiles = [];
-  
-  for(var cssFileIndex = 0; cssFileIndex < document.styleSheets.length; cssFileIndex++) {
-	for(var rules=0; document.styleSheets[cssFileIndex].rules && rules<document.styleSheets[cssFileIndex].rules.length; rules++){
-		if(document.styleSheets[cssFileIndex].rules[rules].type == 3){
-			var foundCSSFile = absolute(document.styleSheets[cssFileIndex].href,document.styleSheets[cssFileIndex].rules[rules].href);
-			foundCSSFiles.push(foundCSSFile);
-		}
-	}
+/* global chrome, $, absolute */
+/* exported getCSSData */
+
+function findEmbeddedCSSURIs () {
+  var foundCSSFiles = []
+
+  for (var cssFileIndex = 0; cssFileIndex < document.styleSheets.length; cssFileIndex++) {
+    for (var rules = 0; document.styleSheets[cssFileIndex].rules && rules < document.styleSheets[cssFileIndex].rules.length; rules++) {
+      if (document.styleSheets[cssFileIndex].rules[rules].type === 3) {
+        var foundCSSFile = absolute(document.styleSheets[cssFileIndex].href, document.styleSheets[cssFileIndex].rules[rules].href)
+        foundCSSFiles.push(foundCSSFile)
+      }
+    }
   }
-  return foundCSSFiles;
+  return foundCSSFiles
 }
 
-
-function getDOMCSSURIs() {
-  var cssURIs = [];
-  for(var cssFileIndex = 0; cssFileIndex < document.styleSheets.length; cssFileIndex++) {
-    cssURIs.push(document.styleSheets[cssFileIndex].href);
+function getDOMCSSURIs () {
+  var cssURIs = []
+  for (var cssFileIndex = 0; cssFileIndex < document.styleSheets.length; cssFileIndex++) {
+    cssURIs.push(document.styleSheets[cssFileIndex].href)
   }
-  return cssURIs;
+  return cssURIs
 }
 
-function getCSSData(cb) {
- console.log('called getCSSData in cssExtractor.js')
-    //a better way to get all stylesheets but we cannot get them as text but instead an object with ruleslist
-    var styleSheetURLs = [];
-    //var styleSheetData = [];
-    var css = {};
+function getCSSData (cb) {
+  console.log('called getCSSData in cssExtractor.js')
+  // A better way to get all stylesheets but we cannot get them as text but instead an object with ruleslist
+  var styleSheetURLs = []
+  // var styleSheetData = []
+  var css = {}
 
-    var domCSSFileURIs = getDOMCSSURIs(); // Synchronous
-    var embeddedCSSFileURIs = findEmbeddedCSSURIs(); // Synchronous
-    styleSheetURLs = domCSSFileURIs.concat(embeddedCSSFileURIs);
+  var domCSSFileURIs = getDOMCSSURIs() // Synchronous
+  var embeddedCSSFileURIs = findEmbeddedCSSURIs() // Synchronous
+  styleSheetURLs = domCSSFileURIs.concat(embeddedCSSFileURIs)
 
+  function getCSSDataAtIndex (ii) {
+    if (ii >= styleSheetURLs.length) {
+      chrome.storage.local.set({'css': css}, function () {
+        cb()
+      })
+      return
+    }
 
-    function getCSSDataAtIndex(ii) {   
-	    if(ii >= styleSheetURLs.length) {		  
-		  chrome.storage.local.set({'css' : css}, function() {
-		    cb();
-		  });
-		  return;
-		}
-   
-	    $.ajax({
-		  url: styleSheetURLs[ii],
-		  dataType: 'text'
-	    }).done(function(cssText){
-	       css[styleSheetURLs[ii]] = cssText;
-		   getCSSDataAtIndex(++ii);
-	    });
-	}
-	 
-	getCSSDataAtIndex(0);         
+    $.ajax({
+      url: styleSheetURLs[ii],
+      dataType: 'text'
+    }).done(function (cssText) {
+      css[styleSheetURLs[ii]] = cssText
+      getCSSDataAtIndex(++ii)
+    })
+  }
+
+  getCSSDataAtIndex(0)
 }
--- js/jsExtractor.js
+++ js/jsExtractor.js
@@ -1,36 +1,35 @@
-function getJSData(cb) {
-  console.log('called getJSdata in jsExtractor.js');
-  
-  var js = {};
-  
-  function getJSDataAtIndex(ii) {   
-    console.log(ii);
-    if(ii >= document.scripts.length) {
-      chrome.storage.local.set({'js': js}, function() {
-        cb();
-      });
-        
-      return;
+/* global chrome, $ */
+
+function getJSData (cb) {
+  console.log('called getJSdata in jsExtractor.js')
+
+  var js = {}
+
+  function getJSDataAtIndex (ii) {
+    console.log(ii)
+    if (ii >= document.scripts.length) {
+      chrome.storage.local.set({'js': js}, function () {
+        cb()
+      })
+
+      return
     }
-    
-    
-    console.log('ajax to ' + document.scripts[ii].src);
-    
+
+    console.log('ajax to ' + document.scripts[ii].src)
+
     $.ajax({
       url: document.scripts[ii].src,
       dataType: 'text'
-    }).done(function(jsText){
-      js[document.scripts[ii].src] = jsText;
-      getJSDataAtIndex(++ii);
-    }).fail(function(xhr, status, err) {
-      console.log('An error occurred fetching '+ document.scripts[ii].src);
-      console.log(err);
-      js[document.scripts[ii].src] = '(An adblocker prevented WARCreate from capturing this file)';
-      getJSDataAtIndex(++ii);
-      
-    });
-    
+    }).done(function (jsText) {
+      js[document.scripts[ii].src] = jsText
+      getJSDataAtIndex(++ii)
+    }).fail(function (xhr, status, err) {
+      console.log('An error occurred fetching ' + document.scripts[ii].src)
+      console.log(err)
+      js[document.scripts[ii].src] = '(An adblocker prevented WARCreate from capturing this file)'
+      getJSDataAtIndex(++ii)
+    })
   }
-  
-  getJSDataAtIndex(0);
+
+  getJSDataAtIndex(0)
 }
