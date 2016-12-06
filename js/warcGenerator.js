774cef32a79b2ce6410d1074178a260ba3bf90d5
Mat Kelly <me@matkelly.com>
2016-12-06 17:56:29 -0500

    Check for undefined, not null is CSS

--- js/warcGenerator.js
+++ js/warcGenerator.js
@@ -346,7 +346,9 @@ function generateWarc(o_request, o_sender, f_callback){
 		  responseHeaders[requestHeader] &&
 		  cssregexp.exec(responseHeaders[requestHeader]) != null)
 		{
-			if(cssURIs === null){break;}
+			if(!cssURIs){
+			  break;
+			}
 			responsesToConcatenate[requestHeader] = "pending";
 			console.log(requestHeader+" is a CSS file");
 			var respHeader = responseHeaders[requestHeader] + CRLF + CRLF;
