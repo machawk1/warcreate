a551840850cba585266176ea033fde32ae6fcbb1
Mat Kelly <me@matkelly.com>
2020-12-23 15:54:34 -0500

    Mv 'code' JS to root for manifest v3, rename across files for #117

--- js/encryptPage.js
+++ js/encryptPage.js
@@ -1,6 +1,3 @@
-// let key = params['k']
-
-// alert($('#cipher'))
 function convertImagesToBase64 () {
   const images = document.getElementsByTagName('img')
   for (let i = 0; i < images.length; i++) {
@@ -11,16 +8,9 @@ function convertImagesToBase64 () {
       console.log(response.farewell)
     })
 
-    // return
-    // Create canvas tag to represent img
     let canvas = document.createElement('canvas')
     canvas.width = img.width
     canvas.height = img.height
-
-    // let context = canvas.getContext('2d')
-    // let newImage = new Image()
-    // newImage.src
-    // context.drawImage(img,0,0)
 
     let fileType = images[i].src.substr(images[i].src.length - 4).toLowerCase()
     console.log(i + ': ' + images[i].src + '  file type: ' + fileType)
@@ -39,33 +29,20 @@ function convertImagesToBase64 () {
 
     try {
       const base64 = canvas.toDataURL(fileType)
-      // alert(base64)
 
       img.src = base64
 
-      // alert(base64)
-      // img.src = base64;//"https://www.google.com/intl/en_com/images/srpr/logo3w.png"
-
-      // chrome.extension.sendRequest({url: img.src})
-      // chrome.extension.getBackgroundPage().adjustImage(img.src)
     } catch (e) {
       console.log(e)
       return
     }
     $(images[i]).replaceWith(img)
   }
-// console.log(document.documentElement.innerHTML)
 }
 
 let scripts = document.getElementsByTagName('script')
 for (let s = 0; s < scripts.length; s++) {
   scripts[s].parentNode.removeChild(scripts[s])
-// console.log(scripts[s].parentNode.innerHTML)
 }
 
 convertImagesToBase64()
-
-// var content = document.documentElement.innerHTML
-// var encryptedContent = $.rc4EncryptStr(content, key)
-// $('body').append('<br />Encrypted Content: ' + encryptedContent)
-// $('body').append('<br />Encrypted Content: ' + content)
